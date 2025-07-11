908853420ded8a1ad029efaa5b2311c02c329275
Jake Hamilton <jakehamtexas@gmail.com>
2025-07-10 23:48:27 -0500

    chore: update flake metadata

--- package.nix
+++ package.nix
@@ -8,9 +8,10 @@ pkgs.rustPlatform.buildRustPackage {
   cargoHash = "sha256-+jB0Wpbj3vdPZ2mZU7muigbYuOzTONz8U0+cAz31VW0=";
 
   meta = {
+    mainProgram = "hyprlang-fmt";
     description = "A formatter for hyprlang";
     homepage = "https://github.com/jkhmtx/hyprlang-fmt";
     license = pkgs.lib.licenses.gpl3;
-    maintainers = [];
+    maintainers = ["jakehamtexas@gmail.com"];
   };
 }
