422e343aaf62632229ccf82102b1e7065eaea685
Jake Hamilton <jakehamtexas@gmail.com>
2025-07-13 18:48:57 -0500

    refactor(core): move projectNamespace to project.nix

--- flake.nix
+++ flake.nix
@@ -24,43 +24,7 @@
 
     projectInputs = {
       inherit pkgs;
-      projectNamespace = {
-        rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain:
-          toolchain.minimal.override {
-            targets = [
-              "x86_64-apple-darwin"
-              "x86_64-unknown-linux-gnu"
-              "aarch64-apple-darwin"
-              "aarch64-unknown-linux-gnu"
-            ];
-            extensions = [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustc-codegen-cranelift-preview"
-              "rustfmt"
-            ];
-          });
-        scripts = {
-          fix = import ./scripts/bin/fix/main.nix projectInputs;
-
-          format = {
-            format-nix = import ./scripts/bin/format-nix/main.nix projectInputs;
-            format-rust = import ./scripts/bin/format-rust/main.nix projectInputs;
-            format-shell = import ./scripts/bin/format-shell/main.nix projectInputs;
-            format-yaml = import ./scripts/bin/format-yaml/main.nix projectInputs;
-          };
-
-          lint = {
-            lint-github-actions = import ./scripts/bin/lint-github-actions/main.nix projectInputs;
-            lint-rust = import ./scripts/bin/lint-rust/main.nix projectInputs;
-            lint-shell = import ./scripts/bin/lint-shell/main.nix projectInputs;
-          };
-
-          local-ci = import ./scripts/bin/local-ci/main.nix projectInputs;
-        };
-      };
+      projectNamespace = import ./project.nix projectInputs;
     };
 
     formatter = pkgs.alejandra;
@@ -68,8 +32,6 @@
     package = import ./package.nix projectInputs;
   in {
     inherit shell;
-    inherit (pkgs.rustPlatform) buildRustPackage;
-    inherit (projectInputs.projectNamespace) rust;
 
     packages."${system}".default = package;
     formatter."${system}" = formatter;
